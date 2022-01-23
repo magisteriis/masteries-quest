a5ee98fe8533c0807de8e2d552859b97dbf6b9a0
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-23 11:29:01 +0100

    Testing the API

---
+++ ChampionMastery.Blazor/Models/SummonerSearch.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChampionMastery.Blazor
+{
+    public class SummonerSearch
+    {
+        [Required]
+        public string? SummonerName { get; set; }
+    }
+}
