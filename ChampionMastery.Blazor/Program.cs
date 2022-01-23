aa11c7f2c4e4183c9aef742e7f5bdf9d38483888
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-23 14:04:48 +0100

    Updated base address format

--- ChampionMastery.Blazor/Program.cs
+++ ChampionMastery.Blazor/Program.cs
@@ -7,7 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-RiotGamesApiHttpClient.BaseAddressFormat = "https://api-riotgames.bolinder.uk/{0}";
+RiotGamesApiHttpClient.BaseAddressFormat = "https://api-riotgames.bolinder.uk/{0}/";
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
