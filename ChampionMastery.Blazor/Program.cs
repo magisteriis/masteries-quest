ba0f690b0f3386664651ec5b88657c793664a9dd
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-23 12:40:15 +0100

    Now using MikaelDui.RiotGames.Client.

--- ChampionMastery.Blazor/Program.cs
+++ ChampionMastery.Blazor/Program.cs
@@ -1,6 +1,7 @@
 using ChampionMastery.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using RiotGames;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,3 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+RiotGamesApiHttpClient.BaseAddressFormat = "https://api-riotgames.bolinder.uk/{0}";
